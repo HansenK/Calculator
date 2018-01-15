24e635228fd8addff38c1136c95c9bf81d525168
Thiago Hansen <34888762+HansenK@users.noreply.github.com>
2018-01-15 14:18:37 -0200

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,35 @@
+document.querySelector('body').addEventListener('keydown', function(event) {
+ 
+    alert( event.keyCode );
+ 
+});
+
+function limpar()
+{
+    document.getElementById("resposta").value='';
+}
+
+function mostrar(valor)
+{
+    var num = document.getElementById("resposta").value +=valor;
+}
+
+function calcular()
+{
+    var res = 0;
+    res = document.getElementById("resposta").value;
+    document.getElementById("resposta").value="";
+    document.getElementById("resposta").value=eval(res);
+    if(document.getElementById("resposta").value=="undefined")
+    {
+        document.getElementById("resposta").value="";
+    }
+}
+
+function press(key)
+{
+    document.getElementById("resposta").focus();
+    if(key==13){
+        calcular();
+     }
+}
